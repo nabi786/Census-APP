05958f3ae675cb7b7675a611b1a6ec1618dd2fb1
Nabi Bux <62851030+nabi786@users.noreply.github.com>
2022-11-20 15:46:14 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 
-// routers
+// routers New 
 const census = require('./routers/census')
 const addNyc = require('./routers/addNyc')
 
